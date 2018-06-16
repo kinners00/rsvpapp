3a6760eb66132d8988a0dee4afc7b42fc111b41d
Dimitri Tischenko <dimitri@puppet.com>
2018-06-16 14:32:11 +0200

    made ports parametrizable

--- rsvp.py
+++ rsvp.py
@@ -102,4 +102,5 @@ def api_rsvp(id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    PORT=os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
