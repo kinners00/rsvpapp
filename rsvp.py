35abfabf2a7e37f69a5692bbc742a9de24b29c56
Dimitri Tischenko <dimitri@puppet.com>
2018-06-16 18:53:00 +0200

    updated styling

--- rsvp.py
+++ rsvp.py
@@ -7,11 +7,11 @@ import json
 
 app = Flask(__name__)
 
-LINK=os.environ.get('LINK', "www.cloudyuga.guru")
-TEXT1=os.environ.get('TEXT1', "CloudYuga")
-TEXT2=os.environ.get('TEXT2', "Garage RSVP")
-LOGO=os.environ.get('LOGO', "https://raw.githubusercontent.com/cloudyuga/rsvpapp/master/static/cloudyuga.png")
-COMPANY=os.environ.get('COMPANY', "CloudYuga Technology Pvt. Ltd.")
+LINK=os.environ.get('LINK', "http://www.puppet.com")
+TEXT1=os.environ.get('TEXT1', "Pipelines Demo")
+TEXT2=os.environ.get('TEXT2', "RSVP App")
+LOGO=os.environ.get('LOGO', "https://raw.githubusercontent.com/timidri/rsvpapp/master/static/puppet.png")
+COMPANY=os.environ.get('COMPANY', "Puppet Inc.")
 
 MONGODB_HOST=os.environ.get('MONGODB_HOST', 'localhost')
 client = MongoClient(MONGODB_HOST, 27017)
